da732507db71c80d4c85d2ebab13b1b52d5c1d52
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-04-26 21:14:41 +0300

    added registration logic

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsImages, BsChatLeftDots } from 'react-icons/bs';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
 import { Link } from 'react-router-dom';
+
+import {createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '@/firebase'
 
 const Register = () => {
   interface FormData {
@@ -11,13 +14,36 @@ const Register = () => {
     password: string;
   }
 
+  const [error, setError] = useState<string | unknown>('');
+  const [authUser, setAuthUser] = useState<boolean>(false)
+
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<FormData>();
 
-  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    try {
+      //Регистрация нового пользователя
+      createUserWithEmailAndPassword(auth, data.email, data.password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          setAuthUser(true)
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    } catch (err) {
+      setError(err);
+      console.log(error)
+    }
+  };
 
   return (
     <div className="w-2/5 bg-bg-card p-10 rounded text-white ">
@@ -37,7 +63,10 @@ const Register = () => {
           placeholder="имя в чате"
           {...register('username', { required: true })}
         />
-        <p className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${errors.username ? '' : 'hidden'}`}>
+        <p
+          className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${
+            errors.username ? '' : 'hidden'
+          }`}>
           {errors.username && 'Это поле обязательно для заполнения'}
         </p>
         <input
@@ -49,7 +78,10 @@ const Register = () => {
             pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
           })}
         />
-        <p className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${errors.email ? '' : 'hidden'}`}>
+        <p
+          className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${
+            errors.email ? '' : 'hidden'
+          }`}>
           {errors.email && 'Введите корректный адрес электронной почты'}
         </p>
         <input
@@ -58,14 +90,20 @@ const Register = () => {
           placeholder="пароль"
           {...register('password', { required: true })}
         />
-        <p className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${errors.password ? '' : 'hidden'}`}>
+        <p
+          className={`mt-1 text-green-500 font-bold bg-black-grey p-2 rounded ${
+            errors.password ? '' : 'hidden'
+          }`}>
           {errors.password && 'Введите пароль'}
         </p>
 
-        <input className="hidden" type="file" id="file" />
-        <label htmlFor="file">
-          <BsImages className="text-blue-400  text-5xl my-6 cursor-pointer" />
-        </label>
+        <div className="hover:text-blue-400 text-white   transition ">
+          <input className="hidden" type="file" id="file" />
+          <label htmlFor="file" className="flex gap-4 items-center cursor-pointer">
+            <BsImages className="  text-5xl my-6 " />
+            <p id="file">Загрузить изображение</p>
+          </label>
+        </div>
         <button
           type="submit"
           className="p-5 rounded bg-black-blue transition w-full hover:bg-button-hover">
